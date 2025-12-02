367d2b61642fc08e720ee1cd4ff6d17449aa4710
Anshuman Dash <ansdash2@gmail.com>
2025-12-02 01:49:37 -0800

    Undo maybe

--- 2025/Python/src/day02.py
+++ 2025/Python/src/day02.py
@@ -1,4 +1,22 @@
+def is_invalid(inp, chunk_size):
+    if chunk_size == 0 or len(inp) % chunk_size:
+        return False
+    for i in range(chunk_size):
+        tmp = {c for c in inp[i::chunk_size]}
+        # print(inp, i, chunk_size, tmp)
+        if len(tmp) != 1:
+            return False
+    return True
+
+
 def main(input):
     p1 = p2 = 0
-
+    for pair in input.split(","):
+        a, b = pair.split("-")
+        print(a, b)
+        for n in range(int(a), int(b) + 1):
+            n = str(n)
+            if is_invalid(n, len(n) // 2):
+                p1 += int(n)
+                print("!!!", n)
     return p1, p2
