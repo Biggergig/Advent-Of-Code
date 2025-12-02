7f2504f741cb3677bce86a3dd1fb054b0bf36422
Anshuman Dash <ansdash2@gmail.com>
2025-12-02 01:32:54 -0800

    Go kms

--- 2025/Python/src/day01.py
+++ 2025/Python/src/day01.py
@@ -6,29 +6,11 @@ def main(input):
         # parses input
         dist = int(line[1:])
         direction = -1 if line[0] == "L" else 1
-
-        print(f"\nLINE: {line} (starting {pos=})")
-
-        print(f"dist is {dist}, turns={dist//100}, remainder={dist%100}")
-        full_rotations = dist // 100
-        p2 += full_rotations
-        if full_rotations:
-            print(f"+{full_rotations} full rotations")
-        dist %= 100
-
-        if direction == -1 and pos == 0 and dist > 0:
-            pos = 100  # don't count moving over
-
-        pos += dist * direction
-        if pos < 0 or pos > 100:
-            p2 += 1
-            print(f"+1 due to overshoot ({pos=})")
-        pos %= 100
-
+        for _ in range(dist):
+            pos += direction
+            pos %= 100
+            if pos == 0:
+                p2 += 1
         if pos == 0:
             p1 += 1
-            if not full_rotations:
-                p2 += 1
-                print("+1 because of ending at 0")
-        print(f"after line: {p2=}")
     return p1, p2
