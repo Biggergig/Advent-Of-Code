fe20a4b5ca2515b237b9ecfbd4b80bed5fff951d
Anshuman Dash <ansdash2@gmail.com>
2025-12-02 01:53:41 -0800

    brute force idgaf

--- 2025/Python/src/day02.py
+++ 2025/Python/src/day02.py
@@ -1,5 +1,5 @@
 def is_invalid(inp, chunk_size):
-    if chunk_size == 0 or len(inp) % chunk_size:
+    if len(inp) % chunk_size:
         return False
     for i in range(chunk_size):
         tmp = {c for c in inp[i::chunk_size]}
@@ -13,10 +13,13 @@ def main(input):
     p1 = p2 = 0
     for pair in input.split(","):
         a, b = pair.split("-")
-        print(a, b)
         for n in range(int(a), int(b) + 1):
             n = str(n)
-            if is_invalid(n, len(n) // 2):
-                p1 += int(n)
-                print("!!!", n)
+            if len(n) % 2 == 0:  # even length
+                if is_invalid(n, len(n) // 2):
+                    p1 += int(n)
+            for chunk_size in range(1, len(n) // 2 + 1):
+                if is_invalid(n, chunk_size):
+                    p2 += int(n)
+                    break
     return p1, p2
