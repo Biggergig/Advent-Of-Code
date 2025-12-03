67fddbc079769df26a4f575750fdb056430dddcf
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-12-03 23:37:06 +0000

    Done!

--- 2025/Python/src/day03.py
+++ 2025/Python/src/day03.py
@@ -1,15 +1,15 @@
 def get_joltage(line):
-    largest = best = float('-inf')
-
-    for c in map(int,reversed(line)):
-        best = max(best,c*10 + largest)
-        largest = max(largest, c)
-
-    return best
+    DP = ['']+[None]*12
+    for c in reversed(line):
+        for i in range(12,0,-1):
+            if DP[i-1] is not None:
+                DP[i]=max(DP[i] or -1,int(c + str(DP[i-1])))
+    return DP[2],DP[12]
 
 def main(input):
-    p2 = 0
-
-    p1 = sum(get_joltage(line) for line in input.splitlines())
-
+    p1 = p2 = 0
+    for line in input.splitlines():
+        a,b = get_joltage(line)
+        p1+=a
+        p2+=b
     return p1, p2
