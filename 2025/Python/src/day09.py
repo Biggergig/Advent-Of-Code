c5233c44c7225751b5d644cce2569c37fff8495e
Anshuman Dash <ansdash2@gmail.com>
2025-12-08 21:29:51 -0800

    Pausing on p2

--- 2025/Python/src/day09.py
+++ 2025/Python/src/day09.py
@@ -1,4 +1,12 @@
 def main(input):
     p1 = p2 = 0
+    points = [tuple(map(int, line.split(","))) for line in input.splitlines()]
+    print(points)
+
+    for i, (x1, y1) in enumerate(points):
+        for x2, y2 in points[i + 1 :]:
+            p1 = max((abs(y1 - y2) + 1) * (abs(x2 - x1) + 1), p1)
+
+    # do they overlap?
 
     return p1, p2
