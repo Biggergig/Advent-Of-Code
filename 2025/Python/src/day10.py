16622182f487033fe2fa85616ff8ef57024fc1d8
Anshuman Dash <ansdash2@gmail.com>
2025-12-14 00:30:06 -0800

    working on it

--- 2025/Python/src/day10.py
+++ 2025/Python/src/day10.py
@@ -1,4 +1,18 @@
+class State:
+    def __init__(self, lights, switches, dist=0):
+        self.lights = lights
+        self.switches = switches
+        self.dist = dist
+
+
 def main(input):
     p1 = p2 = 0
 
+    for line in input.splitlines():
+        lights, *switches, joltage = line.split()
+        lights = int(lights[1:-1][::-1].replace(".", "0").replace("#", "1"), 2)
+        for switch in switches:
+            print(switch)
+        start = State(lights, None)
+        print(lights)
     return p1, p2
