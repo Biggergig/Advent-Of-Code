986e69fe06045a4d1a6435dadd0d669078ab300f
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-12-17 00:56:21 +0000

    no shot that's the solution lmao

--- 2025/Python/src/day12.py
+++ 2025/Python/src/day12.py
@@ -3,18 +3,18 @@ def main(input):
     p1 = p2 = 0
     *raw_pieces, lines = input.split('\n\n')
     pieces = [raw.count('#') for raw in raw_pieces]
-    print(pieces)
+    # print(pieces)
     C = Counter()
     for line in lines.splitlines():
-        print(line,end='\t')
+        # print(line,end='\t')
         lhs,rhs = line.split(':')
         w,h = map(int,lhs.split('x'))
         C[(w,h)]+=1
         counts = list(map(int, rhs.split()))
         if (w*h - sum(a*b for a,b in zip(pieces, counts)))<0:
-            print('X - impossible')
+            # print('X - impossible')
             continue
         p1+=1
-        print()
+        # print()
 
-    return p1, p2
+    return p1, None
