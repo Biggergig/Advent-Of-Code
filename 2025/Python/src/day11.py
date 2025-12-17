dab2c7492db764b3996481daa59af67ba7213c4c
Anshuman Dash <56092891+Biggergig@users.noreply.github.com>
2025-12-17 00:34:46 +0000

    Oops I didn't do dag sorting

--- 2025/Python/src/day11.py
+++ 2025/Python/src/day11.py
@@ -1,28 +1,66 @@
 from collections import deque
+
+def get_paths_from(nodes, source, sink, dag_order):
+    paths = {n:0 for n in nodes}
+    paths['out'] = 0
+    paths[source] = 1
+
+    k = dag_order.index(source)
+    t = dag_order.index(sink)
+
+    for n in dag_order[k:t+1]:
+        for e in nodes[n]:
+            paths[e]+=paths[n]
+    print(paths)
+    return paths[sink]
+
+
+def get_checkpoint_order(dag_order):
+    out = []
+    for n in dag_order:
+        if n in ('fft','dac'):
+            out.append(n)
+    return out
+
+def get_dag_order(nodes):
+    in_edges = {n:0 for n in nodes}
+    for n,edges in nodes.items():
+        for e in edges:
+            in_edges[e]+=1
+    out = []
+    q = deque([n for n,v in in_edges.items() if v == 0])
+    while q:
+        n = q.popleft()
+        out.append(n)
+        for e in nodes[n]:
+            in_edges[e]-=1
+            if in_edges[e] == 0:
+                q.append(e)
+    return out
+
 def main(input):
     p1 = p2 = 0
 
     nodes = {'out':[]}
-    paths = {'out':0}
     for line in input.splitlines():
         n, *out = line.split()
         n = n[:-1]
         nodes[n] = out
-        paths[n] = 0
-    paths['you'] = 1
 
-    done = set()
-    q = deque(['you'])
-    while q:
-        n = q.popleft()
-        if n in done: continue
-        done.add(n)
-        for e in nodes[n]:
-            paths[e]+=paths[n]
-            q.append(e)
-    print(paths)
+    dag_order = get_dag_order(nodes)
 
-    print(nodes)
-    p1 = paths['out']
+    if 'you' in nodes:
+
+        p1 = get_paths_from(nodes,'you','out',dag_order)
+    else:
+        p1 = "N/A"
+
+    if 'svr' in nodes:
+        # This must be a DAG otherwise we would have infinite loops, so just need to find which one is first, and which one is last
+    
+        first,second = get_checkpoint_order(dag_order)
+        print(get_paths_from(nodes, 'svr', 'fft', dag_order))
+    else:
+        p2 = "N/A"
 
     return p1, p2
